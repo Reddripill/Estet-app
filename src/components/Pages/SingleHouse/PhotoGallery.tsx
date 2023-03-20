eae1c8057cc7529b1f5c493d055125793e3ee188
Reddripill <reddripill@mail.ru>
2023-03-20 10:36:23 +0300

    make full functionality to photo gallery

--- src/components/Pages/SingleHouse/PhotoGallery.tsx
+++ src/components/Pages/SingleHouse/PhotoGallery.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styled from 'styled-components'
 import { Image } from '../../../utils/styles'
 
 interface Props {
 	images: string[]
+}
+interface ItemProps extends Props {
+	activeIndex: number;
+	clickHandler: (event: React.MouseEvent<HTMLImageElement>) => void;
 }
 
 const Wrapper = styled.div`
@@ -32,24 +36,64 @@ const ImagesRowItem = styled.li`
 	overflow: hidden;
 	cursor: pointer;
 `
+const GalleryImage = styled(Image)`
+	img {
+		opacity: 0.5;
+		&._active {
+			opacity: 1;
+		}
+	}
+`
+
+const ImageItem = ({ images, activeIndex, clickHandler }: ItemProps) => {
+	return (
+		<>
+			{
+				images.map((image, index) => (
+					<ImagesRowItem key={image}>
+						<GalleryImage>
+							<img
+								src={image}
+								alt="House"
+								onClick={event => clickHandler(event)}
+								className={index === activeIndex ? '_active' : ''}
+								id={`gallery-photo_${index}`}
+							/>
+						</GalleryImage>
+					</ImagesRowItem>
+				))
+			}
+		</>
+	)
+}
 
 const PhotoGallery = ({ images }: Props) => {
+	const [active, setActive] = useState<string>(images[0]);
+	const [activeIndex, setActiveIndex] = useState<number>(0);
+	const mainImgRef = useRef<HTMLImageElement>(null);
+	const clickHandler = (event: React.MouseEvent<HTMLImageElement>) => {
+		const el = event.target as HTMLImageElement;
+		setActive(el.src);
+		setActiveIndex(Number(el.id[el.id.length - 1]))
+	}
+	useEffect(() => {
+		if (mainImgRef.current) {
+			mainImgRef.current.src = active;
+		}
+	}, [active])
 	return (
 		<Wrapper>
 			<MainCanvas>
 				<Image>
-					<img src={images[0]} alt="House" />
+					<img ref={mainImgRef} src={images[0]} alt="House" />
 				</Image>
 			</MainCanvas>
 			<ImagesRow>
-				{images.map(image => (
-					<ImagesRowItem key={image}>
-						<Image>
-							<img src={image} alt="House" />
-						</Image>
-					</ImagesRowItem>
-				))
-				}
+				<ImageItem
+					clickHandler={clickHandler}
+					images={images}
+					activeIndex={activeIndex}
+				/>
 			</ImagesRow>
 		</Wrapper>
 	)
