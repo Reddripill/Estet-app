9300255d81db989277cd1221e050eaeed14d0f6b
Reddripill <reddripill@mail.ru>
2023-06-04 20:22:34 +0300

    add main optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useAppDispatch } from './app/hooks';
-import FilteredElements from './components/UI/Filter/FilteredElements';
-import Home from './components/Pages/MainPage/Page';
-import Layout from './components/UI/Layout';
 import NotFoundPage from './components/UI/NotFoundPage';
 import { fetchProducts } from './features/houses/housesSlice';
 import SingleHousePage from './components/Pages/SingleHouse/SingleHousePage';
-import SignIn from './features/auth/SignIn';
-import SignUp from './features/auth/SignUp';
-import RequireAuth from './features/auth/RequireAuth';
-import PersistAuth from './features/auth/PersistAuth';
-import RootAccountPage from './components/Pages/AccountPages/RootAccountPage';
-import Profile from './components/Pages/AccountPages/Profile';
+// import PersistAuth from './features/auth/PersistAuth';
+const Layout = React.lazy(() => import('./components/UI/Layout'))
+const Home = React.lazy(() => import('./components/Pages/MainPage/Page'))
+const FilteredElements = React.lazy(() => import('./components/UI/Filter/FilteredElements'))
+const SignIn = React.lazy(() => import('./features/auth/SignIn'))
+const SignUp = React.lazy(() => import('./features/auth/SignUp'))
+const RequireAuth = React.lazy(() => import('./features/auth/RequireAuth'))
+const RootAccountPage = React.lazy(() => import('./components/Pages/AccountPages/RootAccountPage'))
+const Profile = React.lazy(() => import('./components/Pages/AccountPages/Profile'))
+
 
 function App() {
 	const dispatch = useAppDispatch();
 	dispatch(fetchProducts());
 	return (
-		<Routes>
-			<Route path='/' element={<Layout />}>
-				<Route index element={<Home />} />
-				<Route
-					path='filteredHouses'
-					element={<FilteredElements />}
-				/>
-				<Route
-					path=':houseId'
-					element={<SingleHousePage />}
-				/>
-				<Route path='auth/signin' element={<SignIn />} />
-				<Route path='auth/signup' element={<SignUp />} />
-			</Route>
-			<Route element={<RequireAuth />}>
-				<Route path='welcome' element={<RootAccountPage />}>
-					<Route path='profile' element={<Profile />} />
+		<Suspense>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route
+						path='filteredHouses'
+						element={<FilteredElements />}
+					/>
+					<Route
+						path=':houseId'
+						element={<SingleHousePage />}
+					/>
+					<Route path='auth/signin' element={<SignIn />} />
+					<Route path='auth/signup' element={<SignUp />} />
 				</Route>
-			</Route>
-			<Route path='*' element={<NotFoundPage />} />
-		</Routes>
+				<Route element={<RequireAuth />}>
+					<Route path='welcome' element={<RootAccountPage />}>
+						<Route path='profile' element={<Profile />} />
+					</Route>
+				</Route>
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+		</Suspense>
 	);
 }
 
